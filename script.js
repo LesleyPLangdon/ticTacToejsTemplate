e3785f4b48424c771570707c1a622a6169eeef61
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2024-11-18 12:13:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+/********************* Function to display game board and set player to x ********************************/
+initializeGame() {
+
+}
+
+/********************* Function handles the player input and places mark on board. This may be completed as more than one function if you choose ********************************/
+playerMove() {
+
+}
+
+/********************* Function checks all rows, columns, and diagonals for three identical symbols ********************************/
+checkWinner() {
+
+}
+
+/********************* Function resets the game board ********************************/
+resetGame() {
+
+}
