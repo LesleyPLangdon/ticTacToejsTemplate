9b84a7e86964fb75e208b3ba6ac789fea1d64fbc
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2024-12-16 13:47:18 -0500

    initialCommitTableBranch

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-
+let player1 = "X";
+let player2 = "O";
+let player = "";
+let turn = 0;
+let square = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 /********************* Function to display game board and set player to x ********************************/
 function initializeGame() {
 
@@ -6,6 +10,7 @@ function initializeGame() {
 
 /********************* Function handles the player input and places mark on board. This may be completed as more than one function if you choose ********************************/
 function playerMove() {
+
 
 }
 
@@ -17,4 +22,17 @@ function checkWinner() {
 /********************* Function resets the game board ********************************/
 function resetGame() {
 
+}
+
+function playSquare(num) {
+    console.log('playSquare ran');
+    
+    if (turn % 2 == 0) {
+        player = player1;
+        turn ++;
+    } else if (turn % 2 == 1) {
+        player = player2;
+        turn ++;
+    }
+    document.getElementById("cell"+num).innerHTML = player;
 }
