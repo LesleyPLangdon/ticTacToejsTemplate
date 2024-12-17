5ea42b4c8ff2bba00c17708c84e5e7a0b32388cd
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2024-12-17 12:43:38 -0500

    win check

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let player1 = "X";
 let player2 = "O";
-let player = "";
+
 let turn = 0;
 let square = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 /********************* Function to display game board and set player to x ********************************/
@@ -9,22 +9,35 @@ function initializeGame() {
 }
 
 /********************* Function handles the player input and places mark on board. This may be completed as more than one function if you choose ********************************/
-function playerMove() {
+function playerMove(num) {
     console.log('playSquare ran');
-    
+    let player = "";
     if (turn % 2 == 0) {
         player = player1;
         turn ++;
+        
     } else if (turn % 2 == 1) {
         player = player2;
         turn ++;
     }
     document.getElementById("cell"+num).innerHTML = player;
+    square[num-1] = player;
+    checkWinner(player);
 
 }
 
 /********************* Function checks all rows, columns, and diagonals for three identical symbols ********************************/
-function checkWinner() {
+function checkWinner(player) {
+    if ((square[0] == square[1] && square[1] == square[2]) ||
+    (square[3] == square[4] && square[4] == square[5]) ||
+    (square[6] == square[7] && square[7] == square[8]) ||
+    (square[0] == square[3] && square[3] == square[6]) ||
+    (square[1] == square[4] && square[4] == square[7]) ||
+    (square[2] == square[5] && square[5] == square[6]) ||
+    (square[0] == square[4] && square[4] == square[8]) ||
+    (square[2] == square[4] && square[4] == square[6])) {
+        document.getElementById("winner").innerHTML = player + "WINS!!!"
+    }
 
 }
 
